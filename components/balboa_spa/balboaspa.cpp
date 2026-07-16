00047e13741800d2b898a427b0629eb0f2814182
eggmister <108530080+eggmister@users.noreply.github.com>
2026-07-16 22:33:39 +0100

    Update balboaspa.cpp
    fix compiler warning for wrong data type

--- components/balboa_spa/balboaspa.cpp
+++ components/balboa_spa/balboaspa.cpp
@@ -32,7 +32,7 @@ void BalboaSpa::update() {
     
     // Check communication timeout with optimized logic
     if (last_received_time > 0 && (now - last_received_time) > COMMUNICATION_TIMEOUT_MS) {
-        ESP_LOGW(TAG, "No communication for %d seconds - marking as dead!", (now - last_received_time) / 1000);
+        ESP_LOGW(TAG, "No communication for %lu seconds - marking as dead!", (now - last_received_time) / 1000);
         //status_set_error("No Communication with Balboa Mainboard!");
         ESP_LOGE(TAG, "No Communication with Balboa Mainboard!");
         status_set_error();
